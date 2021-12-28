0986785148ecf1488eb34a3c32fd159c15126190
David Martinez Garcia <dmg103@gcloud.ua.es>
2021-12-28 19:53:45 +0100

    [main]: Dotfiles gruvbox based on

---
+++ setup.sh
@@ -0,0 +1,16 @@
+sudo pacman -Syyu
+sudo pacman -S yay
+yay -S cmake
+yay -S rofi
+yay -S discord
+yay -S google-chrome
+yay -S spotify
+yay -S visual-studio-code-bin
+yay -S picom
+yay -S gruvbox-dark-gtk
+yay -S gruvbox-dark-icons-gtk
+yay -S powerline-fonts
+
+nitrogen
+
+lxappearance
