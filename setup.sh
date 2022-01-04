fa6742e6c5156bd5aff83a215797be1eee060bfd
David Martinez Garcia <dmg103@gcloud.ua.es>
2022-01-04 22:09:45 +0100

    [main]: Solved conflict with py3status

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ yay -S gruvbox-dark-gtk
 yay -S gruvbox-dark-icons-gtk
 yay -S nerd-fonts-complete
 yay -S flameshot
-yay -S py3status-git
+yay -S py3status
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
